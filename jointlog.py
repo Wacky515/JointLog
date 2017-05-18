90f6dd659a1e4be9e8d81c7f00af13ebf6a4ba98
wacky515 <wacky515@gmail.com>
2017-05-18 15:43:55 +0900

    Init commit develop

---
+++ jointlog.py
@@ -0,0 +1,16 @@
+# import os
+import linecache
+
+def select_tail(data):
+    line_no = sum(1 for line in open(data))
+    tail = str(linecache.getline(data, int(line_no))).strip()
+    print(tail)
+    return tail, line_no
+
+plc_data = select_tail("sample_csv.csv")
+recorder_data = select_tail("sample_csv_b.csv")
+join = plc_data[0] + "," + recorder_data[0]
+
+# print(plc_data)
+# print(recorder_data)
+print(join)
